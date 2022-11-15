91cdce35e053eb630f3ecb69cea1b7cb1b184549
JAZMYNW <95877548+JAZMYNW@users.noreply.github.com>
2022-11-15 11:13:56 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Problem3.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "The number of "words" that contain only letters: " << threeA() << endl;
+    cout << "Shortest pangram: " << threeB() << endl;
+    cout << "Number of valid dictionary words that don't contain first letter of first name: " << threeC() << endl;
+}
