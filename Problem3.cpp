035649096d4e2b4c11b5f53390d771b49ae6883d
JAZMYNW <95877548+JAZMYNW@users.noreply.github.com>
2022-11-16 22:21:29 -0500

    hw6

---
+++ Problem3.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include "Problem3.h"
+#include <fstream>
+#include <string>
+#include <cctype>
+#include <cstring>
+
+using namespace std;
+
+
+
+int threeA(){
+    ifstream txtfile;
+    txtfile.open("TinyData.txt");
+    string word;
+    int count = 0;
+    int lettercount = 0;
+    
+    
+    if(txtfile.is_open()){
+            while(txtfile){
+                txtfile >> word;
+                for (int i = 0; i < word.length(); i++){
+                
+                    if(isalpha(word[i])){ //checks if character is a letter
+                        lettercount++;
+                    } 
+                }
+                if(lettercount == word.length()){ // if the amount of letters is the same amount length incs count
+                    count++;
+                }
+                lettercount = 0;
+            }
+    txtfile.close();
+    return count;
+    }else {
+        cout<<"Error! File could not be opened!"<<endl;
+        return 0;
+    
+    }
+
+}
+
+
+int threeB(){
+    int alphacheck[26] = {0};
+    ifstream txtfile;
+    txtfile.open("TinyData.txt");
+    string word;
+    int count = 0;
+    int letterint;
+    char letter;
+    int sum;
+    int finalcount;
+    
+    
+    if(txtfile.is_open()){
+            while(txtfile){
+                letter = txtfile.get();
+            count++;
+            letterint = tolower(letter);
+            if (isalpha(letter)){
+                if(letterint>=97 && letterint<=122){
+                    alphacheck[letterint-97]=1;
+                    cout<< letter<<endl;
+                }
+                for(int i=0;i<26;i++){
+                    if(alphacheck[i]>=1){
+                        sum++;
+                    } else sum = 0;
+             }
+                if(sum>0)
+                 return count;
+                      }
+            }
+    
+    
+    }else {
+        cout<<"Error! File could not be opened!"<<endl;
+        return 0;
+    
+    }
+}
+
+
+int threeC(){
+ 
+string words[200];
+ifstream txtfile;
+    txtfile.open("TinyData.txt");
+    string word;
+    int count = 0;
+    int lettercount = 0;
+    
+   //checks for words containing only letters like part a 
+    if(txtfile.is_open()){
+            while(txtfile){
+                txtfile >> word;
+                for (int i = 0; i < word.length(); i++){
+                
+                    if(isalpha(word[i])){
+                        lettercount++;
+                    } 
+                }
+                if(lettercount == word.length()){
+                    count++;
+                    words[count-1]=word; //creates array of words
+                }
+                lettercount = 0;
+            }
+    txtfile.close();
+      
+    }else {
+        cout<<"Error! File could not be opened!"<<endl;
+        return 0;
+    
+    }
+
+
+
+ifstream txtfile2;
+txtfile2.open("Dictionary.txt");
+string dicword;
+int maincount = 0;
+
+
+    
+    if(txtfile2.is_open()){
+            while(txtfile2){
+                txtfile2 >> dicword; //opens dictionary file
+//compares strings to current dictionary word, only increments if j isn't in each word
+                for (int i = 0; i < count; i++){
+                    
+                    if((words[i])==(dicword) && (words[i].find("j")||words[i].find("J")) ){ 
+                       
+                     maincount++;
+                    }
+                    
+                } 
+            }
+            
+    return maincount;
+
+    } else {
+        cout<<"Error! File not opened!"<<endl;
+        return 0;
+   
+    } 
+
+}
--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    cout << "The number of "words" that contain only letters: " << threeA() << endl;
+    cout << "The number of \"words\" that contain only letters: " << threeA() << endl;
     cout << "Shortest pangram: " << threeB() << endl;
     cout << "Number of valid dictionary words that don't contain first letter of first name: " << threeC() << endl;
 }
